0d7754a350350418341661aa627f9f0322ffebde
rhigby <ralphhigby@gmail.com>
2025-07-08 11:55:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://acrophobia-frontend.onrender.com",
+    origin: "https://acrophobia-play.onrender.com",
     methods: ["GET", "POST"]
   }
 });
